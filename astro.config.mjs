43ef1530279c7a87f2878d82cf9d389727881089
Matt Moot <matt.moot@outlook.com>
2025-09-01 12:24:46 +1200

    New Blog
    Signed-off-by: Matt Moot <matt.moot@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact()],
-});
+export default defineConfig({});
